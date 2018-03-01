f24426c32bc28655cee022ee2d435510e2948a5f
Johann Schleier-Smith <jssmith@gmail.com>
2018-03-01 22:56:58 +0000

    add configuration script

---
+++ clusterconfig.py
@@ -0,0 +1,162 @@
+import argparse
+import boto3
+import botocore
+import pprint
+import subprocess
+import sys
+
+
+def stack_status(client, stack_name):
+    desc = client.describe_stacks(StackName=stack_name)
+    return desc["Stacks"][0]["StackStatus"]
+
+def run_on_hosts(command_input, timeout=None, failure_expected=False):
+    args = ["pssh", "-h", "host.list.public",
+            "-l", "ec2-user",
+            "-o", "log", "-e", "errlog",
+            "-O", "StrictHostKeyChecking=no",
+            "-I"]
+    if timeout:
+        args += ["-t", str(timeout)]
+    p = subprocess.Popen(args,
+        stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+    command_stdout, command_stderr = p.communicate(input=command_input.encode("utf-8"))
+    if p.returncode and not failure_expected:
+        print(command_stdout)
+        print(command_stderr)
+        raise
+    else:
+        print("success")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Cloud configuration to evaluate a file system")
+    subparsers = parser.add_subparsers(dest="subparser_name", help="sub-command help")
+
+    parser_status = subparsers.add_parser("status", help="Query Cloud Formation stack status")
+    parser_status.add_argument("--stack-name", required=True,
+        help="name of this cloudformation stack")
+
+    parser_deploy = subparsers.add_parser("update", help="Configure a Cloud Formation stack")
+    parser_deploy.add_argument("--stack-name", required=True,
+        help="name of this cloudformation stack")
+    parser_deploy.add_argument("--ssh-key-name", required=True,
+        help="name of the ssh key used to log into ec2 instances")
+    parser_deploy.add_argument("--efs-file-system-id", required=True,
+        help="identifier for the file system")
+    parser_deploy.add_argument("--az-scale-a", type=int, default=1,
+        help="number of test instances in availability zone a")
+    parser_deploy.add_argument("--az-scale-rest", type=int, default=0,
+        help="number of test instances in availability zones b-f")
+
+    parser_instances = subparsers.add_parser("instances", help="Query test host instances")
+    parser_instances.add_argument("--stack-name", required=True,
+        help="name of this cloudformation stack")
+
+    parser_instanceconfig = subparsers.add_parser("instanceconfig",
+        help="Update instances with test software")
+    parser_instanceconfig.add_argument("--stack-name", required=True,
+        help="name of this cloudformation stack")
+    parser_instanceconfig.add_argument("--efs-file-system-id", required=True,
+        help="identifier for the file system")
+
+    args = parser.parse_args()
+
+    stack_name = args.stack_name
+    client = boto3.client("cloudformation")
+    if args.subparser_name == "status":
+        print(stack_status(client, stack_name))
+    elif args.subparser_name == "update":
+        with open("config_efstest.json") as f:
+            template_body = f.read()
+        stack_kwargs = {
+            "StackName": stack_name,
+            "TemplateBody": template_body,
+            "Parameters": [
+                {
+                    "ParameterKey": "KeyName",
+                    "ParameterValue": args.ssh_key_name
+                },
+                {
+                    "ParameterKey": "FileSystem",
+                    "ParameterValue": args.efs_file_system_id
+                },
+                {
+                    "ParameterKey": "AzScaleA",
+                    "ParameterValue": str(args.az_scale_a)
+                },
+                {
+                    "ParameterKey": "PerAzScaleRest",
+                    "ParameterValue": str(args.az_scale_rest)
+                }
+            ],
+            "Capabilities": ["CAPABILITY_IAM"]
+        }
+
+        # check to see whether this stack exists already
+        stack_exists = True
+        try:
+            ds = client.describe_stacks(StackName=stack_name)
+        except botocore.exceptions.ClientError as ex:
+            if str(ex).endswith("does not exist"):
+                stack_exists = False
+            else:
+                raise ex
+        if stack_exists:
+            client.update_stack(**stack_kwargs)
+        else:
+            client.create_stack(**stack_kwargs)
+    elif args.subparser_name == "instances":
+        ec2 = boto3.client("ec2")
+        # TODO - this isn't really the best way to filter, should create a tag
+        filters = [{"Name":"tag:aws:cloudformation:stack-name", "Values":[stack_name]},
+            {'Name':'instance-type', 'Values':['m5.large','m4.large']},
+            {'Name':'instance-state-name', 'Values':['running']}]
+        r = ec2.describe_instances(Filters=filters)
+        instances = list([instance for reservation in r["Reservations"] for instance in reservation["Instances"]])
+        with open("host.list.private", "w") as f:
+            for instance in instances:
+                f.write("%s\n" % instance["PrivateIpAddress"])
+        with open("host.list.public", "w") as f:
+            for instance in instances:
+                f.write("%s\n" % instance["PublicIpAddress"])
+        with open("host.list.subnets", "w") as f:
+            for instance in instances:
+                f.write("%s %s %s\n" % (instance["PrivateIpAddress"], instance["PublicIpAddress"], instance["SubnetId"]))
+        print("have %d load test instances" % len(instances))
+        print("output to host.list.{public,private,subnets}")
+    elif args.subparser_name == "instanceconfig":
+        # TODO gen instances if not already there
+        install_command = (
+            "if [ ! -d /efs ]; then\n"
+            "    sudo mkdir -p /efs\n"
+            "    sudo mount -t nfs4 \\\n"
+            "    -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 \\\n"
+            "    %s.efs.us-east-1.amazonaws.com:/ /efs\n"
+            "fi"
+        ) % args.efs_file_system_id
+        print("running mount command")
+        run_on_hosts(install_command)
+
+        install_fio_command = (
+            "if [ ! -e /usr/local/bin/fio ]; then\n"
+            "    sudo yum install -y libaio-devel git make glibc-devel gcc patch\n"
+            "    git clone https://github.com/axboe/fio\n"
+            "    cd fio\n"
+            "    git checkout fio-3.4\n"
+            "    ./configure\n"
+            "    make\n"
+            "    sudo make install\n"
+            "fi"
+        )
+        print("running fio installation")
+        run_on_hosts(install_fio_command, timeout=300)
+
+        start_fio_command = (
+            "killall -9 fio\n"
+            "nohup /usr/local/bin/fio --server 2>&1 > server.log"
+        )
+        print("running fio server start")
+        run_on_hosts(start_fio_command, timeout=2, failure_expected=True)
+    else:
+        raise
--- scan_fio.py
+++ scan_fio.py
@@ -3,7 +3,6 @@ import json
 import os
 import random
 import re
-import uuid
 import subprocess
 import sys
 import time
@@ -22,7 +21,7 @@ def cleanup(file_names):
         os.unlink(fn)
 
 def gen_job(job_file_name, test_path, block_size, rw_mode, io_depth,
-        warmup_time, run_time):
+        ramp_time, run_time):
     with open(job_file_name,"w") as f:
         config_template = ("[global]\n"
             "name=fio-scan-file\n"
@@ -34,28 +33,62 @@ def gen_job(job_file_name, test_path, block_size, rw_mode, io_depth,
             "numjobs=1\n"
             "time_based=1\n"
             "runtime=%d\n"
+            "ramp_time=%d\n"
             "\n"
             "[file1]\n"
             "size=1G\n"
             "ioengine=libaio\n"
             "iodepth=%d\n")
-        f.write(config_template % (test_path, rw_mode, block_size, run_time, io_depth))
+        f.write(config_template % (test_path, rw_mode, block_size, run_time,
+            ramp_time, io_depth))
+
+def run_fio(host_list_fn, job_fn):
+    fio_args = [args.fio_command,
+        "--client=%s" % host_list_fn,
+        "--output-format=json",
+        job_fn]
+    print("executing", fio_args)
+    p = subprocess.Popen(fio_args,
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    fio_stdout, fio_stderr = p.communicate()
+    if p.returncode:
+        print("fio failed")
+        print(fio_stdout)
+        print(fio_stderr)
+        raise
+    else:
+        m = re.search("\{.*\}", fio_stdout.decode("utf-8"), re.MULTILINE | re.DOTALL)
+        if not m:
+            print("failed to find json in fio output")
+            print(fio_stdout)
+            print(fio_stderr)
+            raise
+        else:
+            return json.loads(m.group(0))
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description="Evaluate a file system")
-    parser.add_argument('result', help='output directory for results')
-    parser.add_argument('--fio-command', default='/usr/local/bin/fio',
-                        help='path to the fio executable')
-    parser.add_argument('--block-sizes', default='4K,32K',
-                        help='number of test fio clients')
-    parser.add_argument('--test-paths', required=True,
-                        help='paths under test')
-    parser.add_argument('--modes', default='randread',
-                        help='paths under test')
-    parser.add_argument('--num-clients', default='1',
-                        help='number of test fio clients')
-    parser.add_argument('--num-iterations', type=int, default=1,
-                        help='number of test fio clients')
+    parser.add_argument("--result", required=True,
+                        help="output directory for results")
+    parser.add_argument("--fio-command", default="/usr/local/bin/fio",
+                        help="path to the fio executable")
+    parser.add_argument("--block-sizes", default="4K,32K",
+                        help="number of test fio clients")
+    parser.add_argument("--test-paths", default="/efs",
+                        help="paths under test")
+    parser.add_argument("--modes", default="randread",
+                        help="paths under test")
+    parser.add_argument("--num-clients", default="1",
+                        help="number of test fio clients")
+    parser.add_argument("--io-depths", default="8",
+                        help="number of concurrent requests per client")
+    parser.add_argument("--num-iterations", type=int, default=1,
+                        help="number of test fio clients")
+    parser.add_argument("--ramp-time", type=int, default=0,
+                        help="warmup time before recording performance")
+    parser.add_argument("--run-time", type=int, default=30,
+                        help="time to run performance test")
+
     args = parser.parse_args()
 
     if not os.path.isdir(args.result):
@@ -64,11 +97,14 @@ if __name__=="__main__":
 
     def splitstrings(arg_str):
         return [x.strip() for x in arg_str.split(",")]
-    test_block_sizes = list(splitstrings(args.num_clients))
+    test_block_sizes = list(splitstrings(args.block_sizes))
     test_clients = list([int(x) for x in splitstrings(args.num_clients)])
     test_paths = list(splitstrings(args.test_paths))
     test_modes = list(splitstrings(args.modes))
+    test_io_depths = list([int(x) for x in splitstrings(args.io_depths)])
     num_iterations = args.num_iterations
+    ramp_time = args.ramp_time
+    run_time = args.run_time
 
     supported_modes = ["read","write","randread","randwrite","randrw"]
     for mode in test_modes:
@@ -76,60 +112,52 @@ if __name__=="__main__":
             print("%s is not a supported mode" % mode)
             sys.exit(1)
 
-    invocation_id = '%016x' % random.getrandbits(64)
+    scan_invocation_id = "%016x" % random.getrandbits(64)
     scan_configuration = {
-        "invocation_id": invocation_id,
+        "scan_invocation_id": scan_invocation_id,
         "paths": test_paths,
-        'block_sizes': test_block_sizes,
-        'modes': test_modes,
-        'clients': test_clients,
-        'iterations': num_iterations
+        "block_sizes": test_block_sizes,
+        "modes": test_modes,
+        "clients": test_clients,
+        "io_depths": test_io_depths,
+        "iterations": num_iterations,
+        "ramp_time": ramp_time,
+        "run_time": run_time
     }
-    print(scan_configuration)
-    sys.exit(0)
-    for i in range(num_iterations):
+    with open(os.path.join(args.result, "scan-%s.json" % scan_invocation_id), "w") as f:
+        json.dump(scan_configuration, f)
+    for i in range(1, num_iterations + 1):
         for test_path in test_paths:
             for bs in test_block_sizes:
                 for rw_mode in test_modes:
-                    for c in test_clients:
-                        experiment_id = uuid.uuid4()
-                        print("experiment %s" % experiment_id)
-                        host_list_fn = os.path.join(args.result, "%s.host.list" % experiment_id)
-                        gen_host_list(host_list_fn, 'host.list', c)
-                        job_fn = os.path.join(args.result, "%s.job" % experiment_id)
-                        gen_job(job_fn, test_path, bs, rw_mode, 8, 0, 3)
-                        experiment_config = {
-                            "mount_path": test_path,
-                            "block_size": bs,
-                            "mode": rw_mode,
-                            "concurrency": c,
-                            "invocation_id": invocation_id,
-                            "iteration": i
-                        }
-                        print("running", experiment_config)
-                        start_time = time.time()
-                        p = subprocess.Popen([args.fio_command,
-                            "--client=%s" % host_list_fn,
-                            "--output-format=json",
-                            job_fn],
-                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                        fio_stdout, fio_stderr = p.communicate()
-                        end_time = time.time()
-                        if p.returncode:
-                            print("fio failed")
-                            print(fio_stderr)
-                        else:
-                            print("result string", fio_stdout.decode("utf-8"))
-                            m = re.search("\{.*\}", fio_stdout.decode("utf-8"), re.MULTILINE | re.DOTALL)
+                    for num_clients in test_clients:
+                        for io_depth in test_io_depths:
+                            experiment_id = "%016x" % random.getrandbits(64)
+                            print("experiment %s" % experiment_id)
+                            host_list_fn = os.path.join(args.result, "%s.host.list" % experiment_id)
+                            gen_host_list(host_list_fn, "host.list.private", num_clients)
+                            job_fn = os.path.join(args.result, "%s.job" % experiment_id)
+                            gen_job(job_fn, test_path, bs, rw_mode, io_depth, ramp_time, run_time)
+                            experiment_config = {
+                                "mount_path": test_path,
+                                "block_size": bs,
+                                "mode": rw_mode,
+                                "clients": num_clients,
+                                "scan_invocation_id": scan_invocation_id,
+                                "iteration": i
+                            }
+                            print("running", experiment_config)
+                            start_time = time.time()
+                            fio_output = run_fio(host_list_fn, job_fn)
+                            end_time = time.time()
                             experiment_output = experiment_config
                             experiment_output["start_time"] = start_time
                             experiment_output["end_time"] = end_time
-                            if not m:
-                                experiment_output["success"] = False
-                                experiment_output["fio_stdout"] = fio_stdout
-                                experiment_output["fio_stderr"] = fio_stderr
+                            if fio_output:
+                                experiment_output["success"] = True
+                                experiment_output["fio_output"] = fio_output
                             else:
-                                experiment_output["fio_output"] = json.loads(m.group(0))
-                            with open(os.path.join(args.result, "%s.json" % experiment_id),"w") as f:
+                                experiment_output["success"] = False
+                            with open(os.path.join(args.result, "%s-%s.json" % (scan_invocation_id, experiment_id)),"w") as f:
                                 json.dump(experiment_output, f)
-                        cleanup([job_fn, host_list_fn])
+                            cleanup([job_fn, host_list_fn])
